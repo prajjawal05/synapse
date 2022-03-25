8b41d78978c8b4e12c6c3b8eace037e547829c68
Sumner Evans <me@sumnerevans.com>
2022-03-25 01:01:09 -0600

    Remove re-assignment of self.clock in UploadResource
    Signed-off-by: Sumner Evans <sumner@beeper.com>

--- synapse/rest/media/upload_resource.py
+++ synapse/rest/media/upload_resource.py
@@ -41,7 +41,6 @@ class UploadResource(DirectServeJsonResource):
         self.clock = hs.get_clock()
         self.auth = hs.get_auth()
         self.max_upload_size = hs.config.media.max_upload_size
-        self.clock = hs.get_clock()
 
     async def _async_render_OPTIONS(self, request: SynapseRequest) -> None:
         respond_with_json(request, 200, {}, send_cors=True)
